463060fbd7b09d7d0a28e62a399448694876a584
smm42 <smm42@uw.edu>
2016-10-11 14:29:22 -0700

    completed exercise 1 and 2

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,19 +2,20 @@
 
 
 # Create a vector of everything you ate for breakfast
-
+breakfast <- c("eggs", "bacon", "hashbrowns", "orange juice")
 
 # Create a vector of everything you ate for lunch
-
+lunch <- c("smoothie")
 
 # Create a list `meals` that has contains your breakfast and lunch
-
+meals <- list(breakfast=breakfast, lunch=lunch)
 
 # Add a `dinner` index to your `meals` list that has what you plan to eat for dinner
-
+meals$dinner <- c("burger", "fries", "drink")
 
 # Extract your 'dinner' element from your list and save it in a vector called 'dinner'
-
+dinner <- meals$dinner
+meals$dinner <- NULL
 
 ### Bonus ### 
 # Create a list that has the number of items you ate for each meal
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,22 +1,22 @@
 # Exercise 2: Creating data frames
 
 # Create a vector of the number of points the Seahawks have scored in each game this season (google "Seahawks")
-
+points.scored <- c(12, 3, 37, 27)
 
 # Create a vector of the number of points the Seahwaks have allowed to be scored against them in each game this season
-
+points.allowed <- c(10, 9, 18, 17)
 
 # Combine your two vectors into a dataframe
-
+points <- data.frame(points.scored = points.scored, points.allowed = points.allowed)
 
 # Create a new column "diff" that is the difference in points
-
+points$diff <- points.scored - points.allowed
 
 # Create a new column "won" which is TRUE if the Seahawks wom
-
+points$won <- points.scored > points.allowed
 
 # Create a vector of the opponents
-
+opponents <- c("Dolphins", "Rams", "49ers", "Jets")
 
 # Assign your dataframe rownames of their opponents
-
+row.names(points) <- opponents
